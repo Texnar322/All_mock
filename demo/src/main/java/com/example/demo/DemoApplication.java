33629afa0d017f80a535b815028adeef1e942eac
Евгений <zheka201589@gmail.com>
2023-09-01 17:49:15 +0300

    ver 1.0

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,11 @@
 package com.example.demo;
-
+import com.example.demo.сontroller.testContoller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan(basePackages = { "com.example.demo.сontroller.testContoller"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
---
+++ demo/src/main/java/com/example/demo/сontroller/testContoller.java
@@ -0,0 +1,38 @@
+package com.example.demo.сontroller;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.nio.file.Files;
+
+@RestController
+public class testContoller {
+    public String rrResponse;
+    @PostConstruct
+    public void init() throws Throwable
+
+    {
+        File file = new File("C:\\Users\\Евгений\\Downloads\\demo\\demo\\src\\main\\resources\\rr.json");
+        rrResponse = new String(Files.readAllBytes(file.toPath()));
+    }
+
+    @GetMapping(value = "/teste")
+    public ResponseEntity<String> CheckMappingRR() throws InterruptedException
+    {
+        return ResponseEntity.ok().header("application/json").body(rrResponse);
+    }
+    //@PostMapping (value = "/tester")
+
+
+
+
+}
+
